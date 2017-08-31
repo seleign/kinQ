7e810727b32c0783ff483cde967be9dcf07426fa
seleign <seleign@hotmail.com>
2017-08-31 15:06:58 +0900

    clear conflict

--- kinQ/src/main/java/us/duia/leejo0531/controller/UserController.java
+++ kinQ/src/main/java/us/duia/leejo0531/controller/UserController.java
@@ -1,5 +1,13 @@
 package us.duia.leejo0531.controller;
 
-public class UserController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+import us.duia.leejo0531.service.UserService;
+
+@Controller
+public class UserController {
+	@Autowired
+	UserService userSvc;
+	
 }
--- kinQ/src/main/java/us/duia/leejo0531/service/UserService.java
+++ kinQ/src/main/java/us/duia/leejo0531/service/UserService.java
@@ -1,5 +1,18 @@
 package us.duia.leejo0531.service;
 
-public class UserService {
+import javax.inject.Inject;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+import us.duia.leejo0531.dao.UserDAO;
+
+@Service
+@Repository
+public class UserService {
+	@Autowired
+	private UserDAO uDao;
+	
+	
 }
