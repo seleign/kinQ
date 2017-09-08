bcbf2c558ecd50f284fccd245bece69279173512
LEEJUNYEON <leejo0531@gmail.com>
2017-09-08 14:33:51 +0900

    답변(Reply)의 VO, DAO, CONTROLLER 작성 및 일부 주석 수정

--- kinQ/src/main/java/us/duia/leejo0531/dao/ReplyDAO.java
+++ kinQ/src/main/java/us/duia/leejo0531/dao/ReplyDAO.java
@@ -1,5 +1,32 @@
 package us.duia.leejo0531.dao;
 
-public interface ReplyDAO {
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+import us.duia.leejo0531.vo.ReplyVO;
+
+/**
+ * 1) 이 DAO는 DB에 질의하는데 사용된다.
+ * 2) 메소드에 절대로 비즈니스 로직을 작성하지 않는다.
+ * 3) 개발에 필요한 메소드를 작성한다.  // 아무나 작성하세요.
+ * @author leejunyeon
+ */
+@Repository
+public class ReplyDAO {
+	
+	@Autowired
+	private SqlSession sqlSession;
+	
+	/**
+	 * DB에 답변을 등록한다. 
+	 * @param replyVO 등록할 답변 VO
+	 * @return 답변 등록 결과
+	 */
+	public int insertReply(ReplyVO replyVO) {
+		ReplyMapper mapper = sqlSession.getMapper(ReplyMapper.class);
+		int result = mapper.insertReply(replyVO);
+		return result;
+	}
+	
 }
--- kinQ/src/main/java/us/duia/leejo0531/dao/ReplyMapper.java
+++ kinQ/src/main/java/us/duia/leejo0531/dao/ReplyMapper.java
@@ -1,5 +1,12 @@
 package us.duia.leejo0531.dao;
 
-public interface ReplyMapper {
+import java.util.ArrayList;
 
+import us.duia.leejo0531.vo.QuestionVO;
+import us.duia.leejo0531.vo.ReplyVO;
+
+public interface ReplyMapper {
+	//여기는 개발에 필요한 사람이 추가하고 커밋하세요.
+	public int insertReply(ReplyVO replyVO);
+	public ArrayList<String> selectReplys();
 }
