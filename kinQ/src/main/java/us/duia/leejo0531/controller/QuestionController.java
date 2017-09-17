974fce2502878c834bdb7f97d994b4b2400c0bbe
LEEJUNYEON <leejo0531@gmail.com>
2017-09-17 23:10:58 +0900

    준연: 실시간 답변 컨트롤러 생성

--- kinQ/src/main/java/us/duia/leejo0531/controller/QuestionController.java
+++ kinQ/src/main/java/us/duia/leejo0531/controller/QuestionController.java
@@ -192,7 +192,7 @@ public class QuestionController {
 	 * @param questionNum 답변할 페이지에 출력시킬 질문의 내용
 	 * @return 실시간 답변페이지
 	 */
-	@RequestMapping(value = "realTimeAnswer", method = RequestMethod.GET)
+	@RequestMapping(value = "realTimeAnswer", method = RequestMethod.POST)
 	public String realTimeAnswerGET(int questionNum, Model model, HttpSession session) {
 		int userNum = (int)session.getAttribute("userNum");
 		QuestionVO question= new QuestionVO( questionNum );
